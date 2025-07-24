e52257b9ebcefcfda27111190604f83bcdc8dfa3
Anushree <anushreers2009@gmail.com>
2025-07-24 11:57:15 -0500

    Initial Commit

---
+++ Navigation./Navigation_App.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_App.swift
+//  Navigation.
+//
+//  Created by Anushree RS on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
